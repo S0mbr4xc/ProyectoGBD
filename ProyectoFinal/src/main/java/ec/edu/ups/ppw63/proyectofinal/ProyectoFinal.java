8e983c1a71fe4089c32099d3db96e0ab25f9a48b
S0mbr4xc <s3._xc@hotmail.com>
2024-02-06 22:10:42 -0500

    Rehacer

---
+++ ProyectoFinal/src/main/java/ec/edu/ups/ppw63/proyectofinal/ProyectoFinal.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package ec.edu.ups.ppw63.proyectofinal;
+
+/**
+ *
+ * @author Esteban Córdova
+ */
+public class ProyectoFinal {
+
+    public static void main(String[] args) {
+        System.out.println("ño");
+    }
+}
